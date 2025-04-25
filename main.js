3f676497804e242ad0f6a29e08bc99038d29a9df
katherinemorgan-ut <katherinemorgan.ut@gmail.com>
2025-04-25 15:56:11 -0600

    adds simple event listener with various DOM manipulation

--- main.js
+++ main.js
@@ -1,27 +1,8 @@
-console.log(window);
-
-// // Single element 
-// const form = document.getElementById('my-form');
-// const querySelector = document.querySelector('h1');
-
-
-// // Multiple element
-// console.log(document.querySelectorAll('.item'));
-// console.log(document.getElementsByClassName('item'));
-
-// const items = document.querySelectorAll('.item');
-
-// items.forEach((item) => console.log(item));
-
-const ul = document.querySelector('.items');
-
-// ul.remove();
-
-// ul.lastElementChild.remove();
-
-ul.firstElementChild.textContent = 'Hello';
-ul.children[1].innerText = 'Brad';
-ul.lastElementChild.innerHTML = '<h1>Hello</h1>'
-
 const btn = document.querySelector('.btn');
-btn.style.background = 'red';
+
+btn.addEventListener('mouseover', (event) => {
+    event.preventDefault(); // to prevent form from submitting
+    document.querySelector('#my-form').style.background = '#ccc';
+    document.querySelector('body').classList.add('bg-dark');
+    document.querySelector('.items').lastElementChild.innerHTML = '<h1>Hello</h1>';
+});
